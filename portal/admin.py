4d89895a8574d9c468920c9cd80efa7405d37213
usarzokirov1968 <usar.zokirov1968@gmail.com>
2026-09-09 07:04:39 +0500

    feat: implement admin bulk subject assignment and dynamic class dropdown

--- portal/admin.py
+++ portal/admin.py
@@ -1,5 +1,16 @@
-from django.contrib import admin
-from .models import School, Teacher, Student, Grade, QuarterGrade, QuarterLock, ClassSubject, UserProfile
+from django.contrib import admin, messages
+from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import path, reverse
+
+from .forms import ClassSubjectAdminForm, BulkClassSubjectForm
+from .models import (
+    School, Teacher, Student, Grade, QuarterGrade, QuarterLock,
+    ClassSubject, UserProfile, normalize_class_name, normalize_subject,
+)
+from .utils import class_numeric_part
+
 
 admin.site.site_header = "Шӯъбаи маорифи ноҳияи Зафаробод"
 admin.site.site_title = "Портали маориф"
@@ -42,10 +53,76 @@ class QuarterGradeAdmin(admin.ModelAdmin):
 
 @admin.register(ClassSubject)
 class ClassSubjectAdmin(admin.ModelAdmin):
+    form = ClassSubjectAdminForm
+    change_form_template = 'admin/portal/classsubject/change_form.html'
+    change_list_template = 'admin/portal/classsubject/change_list.html'
     list_display = ['school', 'class_name', 'subject', 'teacher', 'is_default', 'is_active']
     list_filter = ['school', 'is_default', 'is_active']
     search_fields = ['subject', 'class_name', 'teacher__username']
     list_editable = ['teacher', 'is_active']
+
+    def get_urls(self):
+        info = self.model._meta.app_label, self.model._meta.model_name
+        return [
+            path('school-classes/', self.admin_site.admin_view(self.school_classes_json),
+                 name=f'{info[0]}_{info[1]}_school_classes'),
+            path('bulk-assign/', self.admin_site.admin_view(self.bulk_assign_view),
+                 name=f'{info[0]}_{info[1]}_bulk_assign'),
+        ] + super().get_urls()
+
+    def school_classes_json(self, request):
+        school_id = request.GET.get('school')
+        if not school_id:
+            return JsonResponse([], safe=False)
+        try:
+            school_id = int(school_id)
+        except (ValueError, TypeError):
+            return JsonResponse([], safe=False)
+        school = get_object_or_404(School, pk=school_id)
+        names = set()
+        names.update(ClassSubject.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+        names.update(Student.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+        names = {normalize_class_name(n) for n in names if n}
+        names = sorted(names, key=lambda c: (class_numeric_part(c) or 0, c))
+        return JsonResponse(list(names), safe=False)
+
+    def bulk_assign_view(self, request):
+        if not (request.user.is_superuser or request.user.is_staff):
+            raise PermissionDenied
+        created = None
+        if request.method == 'POST':
+            form = BulkClassSubjectForm(request.POST)
+            if form.is_valid():
+                grade = form.cleaned_data['grade']
+                subject = normalize_subject(form.cleaned_data['subject'])
+                created = 0
+                for school in School.objects.all():
+                    class_names = set()
+                    class_names.update(ClassSubject.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+                    class_names.update(Student.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+                    for cn in class_names:
+                        if str(class_numeric_part(cn)) == grade:
+                            _, was_created = ClassSubject.objects.get_or_create(
+                                school=school,
+                                class_name=normalize_class_name(cn),
+                                subject=subject,
+                                defaults={'is_active': True, 'is_default': False},
+                            )
+                            if was_created:
+                                created += 1
+                self.message_user(
+                    request,
+                    f'{created} сабтҳои "{subject}" барои синфи {grade} илова шуд.',
+                    level=messages.SUCCESS,
+                )
+                return HttpResponseRedirect(reverse('admin:portal_classsubject_changelist'))
+        else:
+            form = BulkClassSubjectForm()
+        return render(request, 'admin/portal/classsubject/bulk_assign.html', {
+            'form': form,
+            'title': 'Бulk Assign Subject by Grade',
+            'created_count': created,
+        })
 
 
 @admin.register(QuarterLock)
--- portal/forms.py
+++ portal/forms.py
@@ -35,3 +35,54 @@ class ClassSubjectForm(forms.ModelForm):
     class Meta:
         model = ClassSubject
         fields = ['school', 'class_name', 'subject', 'is_active']
+
+
+class ClassSubjectAdminForm(forms.ModelForm):
+    class Meta:
+        model = ClassSubject
+        fields = ['school', 'class_name', 'subject', 'is_active']
+
+    class_name = forms.ChoiceField(label='Синф', choices=[], required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        school = self.initial.get('school') or self.data.get('school')
+        if not school and self.instance and self.instance.pk:
+            school = self.instance.school_id
+        try:
+            school = int(school) if school is not None else None
+        except (ValueError, TypeError):
+            school = None
+        if school:
+            self.fields['class_name'].choices = self._class_choices(school)
+        else:
+            self.fields['class_name'].choices = [('', 'Аввал муассисаро интихоб кунед')]
+
+    @staticmethod
+    def _class_choices(school_id):
+        from .models import School, Student, ClassSubject, normalize_class_name
+        from .utils import class_numeric_part
+        try:
+            school = School.objects.get(pk=school_id)
+        except School.DoesNotExist:
+            return [('', 'Муассиса ёфт нашуд')]
+        names = set()
+        names.update(ClassSubject.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+        names.update(Student.objects.filter(school=school).values_list('class_name', flat=True).distinct())
+        names = {normalize_class_name(n) for n in names if n}
+        names = sorted(names, key=lambda c: (class_numeric_part(c) or 0, c))
+        return [('', 'Синфро интихоб кунед...')] + [(n, n) for n in names]
+
+
+class BulkClassSubjectForm(forms.Form):
+    grade = forms.ChoiceField(label='Синф (рақам)', choices=[], required=True)
+    subject = forms.ChoiceField(label='Фан', choices=[], required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        from django.conf import settings
+        from .utils import official_subjects
+        grades = sorted(settings.TJC_SUBJECTS.keys(), key=lambda x: int(x))
+        self.fields['grade'].choices = [('', 'Рақами синфро интихоб кунед...')] + [(g, g) for g in grades]
+        subjects = sorted(official_subjects())
+        self.fields['subject'].choices = [('', 'Фанро интихоб кунед...')] + [(s, s) for s in subjects]
